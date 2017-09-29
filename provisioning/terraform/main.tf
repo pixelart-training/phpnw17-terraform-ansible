178e3919c61c3cb48029c1cc9c627f0cb388394c
Patrik Karisch <patrik@karisch.guru>
2017-09-29 08:30:00 +0000

    Step 03: Resolve dynamic data

--- provisioning/terraform/main.tf
+++ provisioning/terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
-  ami             = "${var.aws_ami}"
+  ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.my_key.key_name}"
   security_groups = ["${aws_security_group.my_security_group.name}"]
